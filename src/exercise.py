687abb35c61ea01ee45d6ba048cfce972fc20948
cabosh <callum.bosher@gmail.com>
2020-06-03 15:36:49 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    password = ("Caput Draconis")
+
+    guess = input("What is the password? ")
+    if guess == password:
+        print("Welcome!")
+    else:
+        print("Off with you!")
 
 if __name__ == '__main__':
     main()
